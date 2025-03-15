4dd7ffaf5d24aa38cbde95a0cd7bfdf01f8ee1ae
Himanish kochhar <himanish.kochhar@gmail.com>
2025-03-15 11:23:45 +0530

    getting all author list and individual author using id

--- src/routes/author.ts
+++ src/routes/author.ts
@@ -2,8 +2,28 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+const authors = [
+  {
+    id: '1',
+    name: 'jijlu'
+  },
+  {
+    id: '2',
+    name: 'kumar'
+  }
+]
+
 app.get('/', c => {
-  return c.text('Hello to authors route api')
+  // Getting all the authors as json
+  return c.json(authors)
+})
+app.get('/:id', c => {
+  const id = c.req.param('id')
+  const author = authors.find(c => c.id === id)
+  if (author == null) {
+    return c.json({ error: 'Author not exist' })
+  }
+  return c.json(author)
 })
 
 export default app
