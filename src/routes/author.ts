4c128d685a2e424df395b34aff398e4cba0f1f23
Himanish kochhar <himanish.kochhar@gmail.com>
2025-03-16 09:14:17 +0530

    To add the new author added the post request

--- src/routes/author.ts
+++ src/routes/author.ts
@@ -1,11 +1,14 @@
+import { sValidator } from '@hono/standard-validator'
 import { Hono } from 'hono'
+import z from 'zod'
 
 const app = new Hono()
 
 const authors = [
   {
     id: '1',
-    name: 'jijlu'
+    name: 'jijlu',
+    brithday: new Date()
   },
   {
     id: '2',
@@ -13,17 +16,33 @@ const authors = [
   }
 ]
 
+const createAuthorSchema = z.object({
+  name: z.string().min(1),
+  birthday: z.coerce.date().optional()
+})
+
 app.get('/', c => {
   // Getting all the authors as json
   return c.json(authors)
 })
+
 app.get('/:id', c => {
   const id = c.req.param('id')
   const author = authors.find(c => c.id === id)
+  // finding the author using an id an returning the author details
   if (author == null) {
-    return c.json({ error: 'Author not exist' })
+    return c.json({ error: 'Author not exist' }, 404)
   }
   return c.json(author)
+})
+
+app.post('/', sValidator('json', createAuthorSchema), c => {
+  const data = c.req.valid('json')
+  // creating the new author using post request
+  const author = { id: crypto.randomUUID(), ...data }
+  authors.push(author)
+
+  return c.json(author, 201) // 201 for succesfuly save the author
 })
 
 export default app
