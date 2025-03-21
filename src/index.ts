d4ccfed14bbbdbca48cf13d29345489c6e6e3f39
Himanish kochhar <himanish.kochhar@gmail.com>
2025-03-21 08:13:04 +0530

    safely parsing the varibles from the env ts file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import authorRoutes from './routes/author.ts'
+import { env } from './data/env.ts'
 const app = new Hono()
 
 app.route('/authors', authorRoutes)
@@ -8,7 +9,7 @@ app.route('/authors', authorRoutes)
 serve(
   {
     fetch: app.fetch,
-    port: 3000
+    port: env.PORT
   },
   info => {
     console.log(`Server is running on http://localhost:${info.port}`)
