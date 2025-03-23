6d04a3790c324bdacdde92924e7da52a157974b3
Himanish kochhar <himanish.kochhar@gmail.com>
2025-03-23 08:39:56 +0530

    In db.ts changes for the schema, newer way in which relations is used

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,7 +1,9 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
 import { env } from '../data/env.ts'
+import { relations } from './relations.ts'
 
 export const db = drizzle({
+  relations,
   connection: {
     user: env.DB_USER,
     password: env.DB_PASSWORD,
