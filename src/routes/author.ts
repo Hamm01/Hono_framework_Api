b4bd17c051f572083b8f88eaf49ba45bc28e13cb
Himanish kochhar <himanish.kochhar@gmail.com>
2025-03-24 20:45:11 +0530

    Added the endpoint in author route to get author using id

--- src/routes/author.ts
+++ src/routes/author.ts
@@ -7,8 +7,18 @@ const app = new Hono()
 
 app.get('/', async c => {
   const authors = await db.query.AuthorTable.findMany()
-  //getting all the authors list from authortable in database
+  //getting all the authors list from author table in database
   return c.json(authors)
+})
+
+app.get('/:id', async c => {
+  const id = c.req.param('id')
+  const author = await db.query.AuthorTable.findFirst({ where: { id: id } })
+  // finding the author using an id in the db query
+  if (author == null) {
+    return c.json({ error: 'Author not exist' }, 404)
+  }
+  return c.json(author)
 })
 
 export default app
