26417e06ab0560cdc75da5dde81570d1a53e2f06
Himanish kochhar <himanish.kochhar@gmail.com>
2025-03-31 06:26:32 +0530

    verify password for scrypt method created

--- src/lib/encrypt.ts
+++ src/lib/encrypt.ts
@@ -1,9 +1,17 @@
-import { randomBytes, scrypt } from 'node:crypto'
+import { randomBytes, scrypt, timingSafeEqual } from 'node:crypto'
 
 export async function hashPassword(password: string) {
   const salt = randomBytes(16).toString('hex')
   const hash = await scryptAsync(password, salt)
   return `${salt}:${hash.toString('hex')}`
+}
+
+export async function verifyPassword(password: string, storedHash: string) {
+  const [salt, hashHex] = storedHash.split(':')
+  console.log()
+  const stored = Buffer.from(hashHex, 'hex')
+  const derived = await scryptAsync(password, salt)
+  return timingSafeEqual(stored, derived)
 }
 
 async function scryptAsync(password: string, salt: string) {
