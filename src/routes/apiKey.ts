4a92bb7c22db633f9992dea8ecf5b90dab502c04
Himanish kochhar <himanish.kochhar@gmail.com>
2025-04-02 17:44:56 +0530

    Added the new endpoint to check the keys returned by that user token

--- src/routes/apiKey.ts
+++ src/routes/apiKey.ts
@@ -4,15 +4,32 @@ import { sValidator } from '@hono/standard-validator'
 import { db } from '../db/db.ts'
 import { UserTable } from '../db/schema.ts'
 import { hashPassword, verifyPassword } from '../lib/encrypt.ts'
-import { jwt, sign } from 'hono/jwt'
+import { jwt } from 'hono/jwt'
 import { env } from '../data/env.ts'
-
-const app = new Hono<JwtEnv>()
 
 type JwtEnv = {
   Variables: {
     jwtPayload: { sub: string; email: string; exp: number }
   }
 }
+// Added new jwtenv type to access the payload as variable in endpoints
+const app = new Hono<JwtEnv>()
+
+app.use(jwt({ secret: env.JWT_SECRET, alg: 'HS256' }))
+
+app.get('/', async c => {
+  const { sub: userId } = c.var.jwtPayload
+
+  const keys = await db.query.ApiKeyTable.findMany({
+    where: { userId },
+    columns: {
+      id: true,
+      name: true,
+      keyPrefix: true,
+      createdAt: true
+    }
+  })
+  return c.json(keys)
+})
 
 export default app
