8f938b38ae2ed9eddc39e877381fd1c7aeaae1b4
Himanish kochhar <himanish.kochhar@gmail.com>
2025-04-04 18:32:12 +0530

    Added the delete endpoint to remove the apikeys

--- src/routes/apiKey.ts
+++ src/routes/apiKey.ts
@@ -6,6 +6,7 @@ import { ApiKeyTable, UserTable } from '../db/schema.ts'
 import { generateApiKey, hashPassword, verifyPassword } from '../lib/encrypt.ts'
 import { jwt } from 'hono/jwt'
 import { env } from '../data/env.ts'
+import { and, eq } from 'drizzle-orm'
 
 type JwtEnv = {
   Variables: {
@@ -41,13 +42,25 @@ app.post('/', sValidator('json', createKeySchema), async c => {
   const { name } = await c.req.valid('json')
   const { hash, prefix, raw } = generateApiKey()
 
-  // As basis of jwt token we get the user id and then genrated random api key with hash and prefix, that api-key will store in api key table for that particualr user, so that user may contains many api keys
+  // As basis of jwt token we get the user id and then genrated random api key with hash and prefix,
+  //  that api-key will store in api key table for that particualr user, so that user may contains many api keys
   const [apiKey] = await db
     .insert(ApiKeyTable)
     .values({ name, userId, keyHash: hash, keyPrefix: prefix })
     .returning({ id: ApiKeyTable.id })
 
   return c.json({ key: raw, id: apiKey.id }, 201)
+})
+
+app.delete('/:id', sValidator('json', createKeySchema), async c => {
+  const { sub: userId } = c.var.jwtPayload
+  const id = c.req.param('id')
+  // Deleting the api key for the userid
+  await db
+    .delete(ApiKeyTable)
+    .where(and(eq(ApiKeyTable.id, id), eq(ApiKeyTable.userId, userId)))
+
+  return c.body(null, 204)
 })
 
 export default app
