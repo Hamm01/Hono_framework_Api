50ba577f9c0dd362587721acca1839732bfe8cca
Himanish kochhar <himanish.kochhar@gmail.com>
2025-04-04 09:04:00 +0530

    Created end point for creating apikeys for the user logged in

--- src/routes/apiKey.ts
+++ src/routes/apiKey.ts
@@ -2,8 +2,8 @@ import { Hono } from 'hono'
 import z from 'zod'
 import { sValidator } from '@hono/standard-validator'
 import { db } from '../db/db.ts'
-import { UserTable } from '../db/schema.ts'
-import { hashPassword, verifyPassword } from '../lib/encrypt.ts'
+import { ApiKeyTable, UserTable } from '../db/schema.ts'
+import { generateApiKey, hashPassword, verifyPassword } from '../lib/encrypt.ts'
 import { jwt } from 'hono/jwt'
 import { env } from '../data/env.ts'
 
@@ -14,12 +14,16 @@ type JwtEnv = {
 }
 // Added new jwtenv type to access the payload as variable in endpoints
 const app = new Hono<JwtEnv>()
+const createKeySchema = z.object({
+  name: z.string().min(1).max(255)
+})
 
 app.use(jwt({ secret: env.JWT_SECRET, alg: 'HS256' }))
 
 app.get('/', async c => {
   const { sub: userId } = c.var.jwtPayload
 
+  // This will list all the api keys that are linked to that particualar user, that userid we got from the jwt token we recieved
   const keys = await db.query.ApiKeyTable.findMany({
     where: { userId },
     columns: {
@@ -30,6 +34,20 @@ app.get('/', async c => {
     }
   })
   return c.json(keys)
+})
+
+app.post('/', sValidator('json', createKeySchema), async c => {
+  const { sub: userId } = c.var.jwtPayload
+  const { name } = await c.req.valid('json')
+  const { hash, prefix, raw } = generateApiKey()
+
+  // As basis of jwt token we get the user id and then genrated random api key with hash and prefix, that api-key will store in api key table for that particualr user, so that user may contains many api keys
+  const [apiKey] = await db
+    .insert(ApiKeyTable)
+    .values({ name, userId, keyHash: hash, keyPrefix: prefix })
+    .returning({ id: ApiKeyTable.id })
+
+  return c.json({ key: raw, id: apiKey.id }, 201)
 })
 
 export default app
