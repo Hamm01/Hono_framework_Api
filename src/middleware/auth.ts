7ece58941f6b4239ffd5572dd851112f2c3a2cd1
Himanish kochhar <himanish.kochhar@gmail.com>
2025-04-07 10:52:09 +0530

    Exported the type Apikey

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,7 +3,7 @@ import { ApiKeyTable, type UserTable } from '../db/schema.ts'
 import { hashApiKey } from '../lib/encrypt.ts'
 import { db } from '../db/db.ts'
 
-type ApiKeyEnv = {
+export type ApiKeyEnv = {
   Variables: {
     apiKeyUser: Pick<typeof UserTable.$inferSelect, 'id' | 'role' | 'email'>
   }
