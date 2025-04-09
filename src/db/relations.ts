223f9aacac0f566495aeb55c4a71c1c090dd9528
Himanish kochhar <himanish.kochhar@gmail.com>
2025-04-09 10:05:40 +0530

    Created the relationship for booktable to author and user

--- src/db/relations.ts
+++ src/db/relations.ts
@@ -5,7 +5,24 @@ export const relations = defineRelations(schema, r => ({
   ApiKeyTable: {
     user: r.one.UserTable({ from: r.ApiKeyTable.userId, to: r.UserTable.id })
   },
+  AuthorTable: {
+    books: r.many.BookTable()
+    // author have one to may relationship
+  },
+  BookTable: {
+    author: r.one.AuthorTable({
+      from: r.BookTable.authorId,
+      to: r.AuthorTable.id
+    }),
+    // In book table one book is relation to one author and same as to one user who created the book in db
+    addedByUser: r.one.UserTable({
+      from: r.BookTable.addedBy,
+      to: r.UserTable.id
+    })
+  },
   UserTable: {
-    apiKeys: r.many.ApiKeyTable()
+    apiKeys: r.many.ApiKeyTable(),
+    booksAdded: r.many.BookTable()
+    // user with as many api keys , and many books he can add, one to many relationship
   }
 }))
